13aec6e9cb0e0aa3032768290bf3211444bc3051
Ansell Maximilian <ansellmaximilian@gmail.com>
2024-01-08 06:56:51 +0700

    mobile responsive

--- app/page.tsx
+++ app/page.tsx
@@ -45,12 +45,12 @@ export default function Home() {
           className="flex"
           style={{ backgroundImage: `url('${bg.src}')` }}
         >
-          <div className="max-w-5xl mx-auto container flex">
-            <div className="py-28 px-4 bg-contain bg-no-repeat bg-right w-6/12 space-y-4">
-              <h1 className="text-4xl font-bold">
+          <div className="max-w-5xl mx-auto container flex flex-col md:flex-row py-8 md:py-28 gap-4 md:gap-0">
+            <div className="px-4 bg-contain bg-no-repeat bg-right md:w-6/12 space-y-4 text-center md:text-left order-2 md:order-1">
+              <h1 className="text-2xl md:text-4xl font-bold">
                 Connecting Through Decentralization
               </h1>
-              <p className=" text-xl font-medium tracking-tighter text-secondary-foreground">
+              <p className="md:text-xl font-medium tracking-tighter text-secondary-foreground">
                 Utilize the brand new Web 5.0 to revolutionize your online
                 social media game! Unlock the full potential of the new era of
                 the internet to protect your privacy and own your data.
@@ -67,9 +67,11 @@ export default function Home() {
               )}
             </div>
             <div
-              style={{ backgroundImage: `url('${hero.src}')` }}
-              className="grow bg-no-repeat bg-right bg-contain"
-            ></div>
+              // style={{ backgroundImage: `url('${hero.src}')` }}
+              className="md:w-1/2 w-full order-1 md:order-2 flex items-center justify-center"
+            >
+              <Image src={hero} className="w-full" alt="hero" />
+            </div>
           </div>
         </section>
         <section
@@ -78,7 +80,7 @@ export default function Home() {
         >
           <div className="max-w-5xl mx-auto container">
             <div className="text-center">
-              <h2 className="font-bold text-3xl">
+              <h2 className="font-bold text-2xl md:text-3xl">
                 What&apos;s Cryptic Hearts?
               </h2>
               <p className="text-lg font-light">
@@ -94,7 +96,7 @@ export default function Home() {
                 className="w-[300px]"
               />
             </div>
-            <div className="flex flex-col gap-2 text-center text-xl tracking-tight">
+            <div className="flex flex-col gap-2 text-center md:text-xl tracking-tight">
               <p>
                 Cryptic Hearts is a revolutionary social media platform built on
                 top of the brand new{" "}
@@ -121,21 +123,21 @@ export default function Home() {
         <section className="py-16">
           <div className="max-w-5xl mx-auto container">
             <div className="text-center">
-              <h2 className="font-bold text-3xl">Features</h2>
+              <h2 className="font-bold text-2xl md:text-3xl">Features</h2>
               <p className="text-lg font-light">Take Advantage of Web 5.0</p>
             </div>
             <div className="mt-8 space-y-16">
-              <div className="flex items-center">
+              <div className="flex flex-col md:flex-row items-center">
                 <Image
                   src={profileSS}
-                  className="h-96 w-auto"
+                  className="w-full md:h-96 md:w-auto"
                   alt="profile page"
                 />
-                <div className="p-4">
-                  <h3 className="text-2xl font-semibold tracking-tight mb-2">
+                <div className="p-4 text-center md:text-left">
+                  <h3 className="text-xl md:text-2xl font-semibold tracking-tight mb-2">
                     Your Own Private Profile
                   </h3>
-                  <p className="text-xl tracking-tight font-light mb-4">
+                  <p className="md:text-xl tracking-tight font-light mb-4">
                     Using Web 5.0 technologies, you&apos;ll be able to{" "}
                     <span className="font-semibold">own</span> your profile.
                     Unless you chose otherwise, you will be the only one able to
@@ -146,12 +148,12 @@ export default function Home() {
                   </Link>
                 </div>
               </div>
-              <div className="flex items-center bg-[#FFEC19] p-8 rounded-md">
-                <div className="pr-2">
-                  <h3 className="text-2xl font-semibold tracking-tight mb-2">
+              <div className="flex flex-col md:flex-row items-center bg-[#FFEC19] p-8 rounded-md">
+                <div className="pr-2 text-center md:text-left order-2 md:order-1">
+                  <h3 className="text-xl md:text-2xl font-semibold tracking-tight mb-2">
                     Connect with People
                   </h3>
-                  <p className="text-xl tracking-tight font-light mb-4">
+                  <p className="md:text-xl tracking-tight font-light mb-4">
                     Follow and get followed.{" "}
                     <span className="font-semibold">Selectively</span> pick and
                     choose what part of your private profile to share and with
@@ -163,21 +165,21 @@ export default function Home() {
                 </div>
                 <Image
                   src={connectSS}
-                  className="h-96 w-auto"
+                  className="w-full md:h-96 md:w-auto order-1 md:order-2"
                   alt="connect page"
                 />
               </div>
-              <div className="flex items-center">
+              <div className="flex flex-col md:flex-row items-center">
                 <Image
                   src={postsSS}
-                  className="h-96 w-auto"
+                  className="w-full md:h-96 md:w-auto"
                   alt="profile page"
                 />
-                <div className="p-4">
-                  <h3 className="text-2xl font-semibold tracking-tight mb-2">
+                <div className="p-4 text-center md:text-left">
+                  <h3 className="text-xl md:text-2xl font-semibold tracking-tight mb-2">
                     Selective Posting
                   </h3>
-                  <p className="text-xl tracking-tight font-light mb-4">
+                  <p className="md:text-xl tracking-tight font-light mb-4">
                     Instead of posting into some centralized database, own your
                     posts and send them to{" "}
                     <span className="font-semibold">specific</span> recipients
